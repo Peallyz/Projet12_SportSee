31a087369616754e9b0a8ea3716303d36819f5d7
Peally <pieclandre@gmail.com>
2023-06-09 20:19:48 +0200

    feat : create all routes and display Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import WorkInProgress from "./Pages/WorkInProgress";
+import Error from "./Pages/Error";
+import Header from "./Components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:user" element={<Dashboard />} />
+          <Route path="/:user/profile" element={<WorkInProgress />} />
+          <Route path="/:user/settings" element={<WorkInProgress />} />
+          <Route path="/:user/community" element={<WorkInProgress />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
