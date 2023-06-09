65de07b6357e28c266441e81cde3d2de1cec7d96
Peally <pieclandre@gmail.com>
2023-06-09 20:21:38 +0200

    feat : create user select component for homepage

---
+++ src/Components/UserSelect/UserSelect.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const UserSelect = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
+};
+
+UserSelect.propTypes = {
+  data: PropTypes.object,
+};
+
+export default UserSelect;
