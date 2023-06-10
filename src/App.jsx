54a1f4b9f94a30a9c9c24c32117a8467ef89d926
Peally <pieclandre@gmail.com>
2023-06-10 12:49:10 +0200

    feat : update routes with path and userID

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import Dashboard from "./Pages/Dashboard";
 import WorkInProgress from "./Pages/WorkInProgress";
 import Error from "./Pages/Error";
 import Header from "./Components/Header";
+import { useState } from "react";
 
 function App() {
+  const [path, setPath] = useState(window.location.pathname.split("/")[1]);
+
   return (
-    <>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:user" element={<Dashboard />} />
-          <Route path="/:user/profile" element={<WorkInProgress />} />
-          <Route path="/:user/settings" element={<WorkInProgress />} />
-          <Route path="/:user/community" element={<WorkInProgress />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header user={path} />
+      <Routes>
+        <Route path="/" element={<Home setPath={setPath} />} />
+        <Route path="/:user" element={<Dashboard />} />
+        <Route path="/:user/profile" element={<WorkInProgress />} />
+        <Route path="/:user/settings" element={<WorkInProgress />} />
+        <Route path="/:user/community" element={<WorkInProgress />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
