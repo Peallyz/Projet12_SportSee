b2bda3237c92cd52093c1980615314395d68b8c5
Peally <pieclandre@gmail.com>
2023-06-10 12:51:24 +0200

    feat : create Link into user

--- src/Components/UserSelect/UserSelect.jsx
+++ src/Components/UserSelect/UserSelect.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-const UserSelect = ({ data }) => {
+const UserSelect = ({ data, setPath }) => {
   console.log(data);
   return (
     <div>
-      <h2></h2>
+      <NavLink to={`/${data.data.id}`} onClick={() => setPath(data.data.id)}>
+        Choisir : {data.data.userInfos.firstname} {data.data.userInfos.lastname}
+      </NavLink>
     </div>
   );
 };
 
 UserSelect.propTypes = {
   data: PropTypes.object,
+  setPath: PropTypes.func,
 };
 
 export default UserSelect;
