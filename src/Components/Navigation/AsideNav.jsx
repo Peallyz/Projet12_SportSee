7b2db9e4b89ac2910fef1bda146bf5146dadcd38
Peally <pieclandre@gmail.com>
2023-06-11 20:52:43 +0200

    feat : display link for each icon

--- src/Components/Navigation/AsideNav.jsx
+++ src/Components/Navigation/AsideNav.jsx
@@ -1,5 +1,28 @@
-const AsideNav = () => {
-  return <div></div>;
+import { PropTypes } from "prop-types";
+import AsideLink from "./AsideLink";
+
+const AsideNav = ({ path }) => {
+  const imgs = [
+    { url: "/src/assets/iconMedidation.svg", alt: "Meditation" },
+    { url: "/src/assets/iconSwiming.svg", alt: "Natation" },
+    { url: "/src/Assets/iconBike.svg", alt: "Vélo" },
+    { url: "/src/assets/iconDumbBell.svg", alt: "Musculation" },
+  ];
+  return (
+    <nav className="aside__nav">
+      <ul>
+        {imgs.map((img) => {
+          <li>
+            <AsideLink img={img} path={path} />
+          </li>;
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+AsideNav.propTypes = {
+  path: PropTypes.string,
 };
 
 export default AsideNav;
