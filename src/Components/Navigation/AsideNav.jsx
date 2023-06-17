cc09ac4afb012cae2db650dac35a63187bb7b881
Peally <pieclandre@gmail.com>
2023-06-17 20:25:56 +0200

    fix : forgotten key on map

--- src/Components/Navigation/AsideNav.jsx
+++ src/Components/Navigation/AsideNav.jsx
@@ -1,7 +1,7 @@
 import { PropTypes } from "prop-types";
 import AsideLink from "./AsideLink";
 
-const AsideNav = ({ path }) => {
+const AsideNav = () => {
   const imgs = [
     { url: "/src/assets/iconMeditation.svg", alt: "Meditation" },
     { url: "/src/assets/iconSwiming.svg", alt: "Natation" },
@@ -12,9 +12,9 @@ const AsideNav = ({ path }) => {
     <nav className="aside__nav">
       <ul>
         {imgs.map((img) => (
-          <>
-            <AsideLink img={img} path={path} />
-          </>
+          <li key={img.alt}>
+            <AsideLink img={img} />
+          </li>
         ))}
       </ul>
     </nav>
