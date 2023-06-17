1f93ea4368d5b609e3ec28c0b2dbeda375c288c2
Peally <pieclandre@gmail.com>
2023-06-17 20:24:53 +0200

    refacto : move display logic into MainNav component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,13 @@
 import MainNav from "./Navigation/MainNav";
-import PropTypes from "prop-types";
 
-const Header = ({ user }) => {
+const Header = () => {
   return (
     <header>
       <img src="../src/assets/logo.svg" alt="Logo de sportsee" />
-      {user ? (
-        <>
-          <MainNav user={user} />
-        </>
-      ) : (
-        <h1>Home</h1>
-      )}
+
+      <MainNav />
     </header>
   );
-};
-
-Header.propTypes = {
-  user: PropTypes.string,
 };
 
 export default Header;
