40e70f9f2867c599ddfcbe534507faaf9ec1e967
Peally <pieclandre@gmail.com>
2023-06-18 21:17:29 +0200

    fix : useMatch instead of useParams to handle nav display

--- src/Components/Navigation/MainNav.jsx
+++ src/Components/Navigation/MainNav.jsx
@@ -1,30 +1,37 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMatch } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 
 const MainNav = () => {
   // const { user } = useParams();
-  const { pathname } = useLocation();
-  console.log();
+  const userPage = useMatch("/:user/*");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (userPage?.params?.user != "12" && userPage?.params?.user != "18") {
+      navigate("/error");
+    }
+  }, []);
 
-  if (pathname.split("/")[1] === "") {
-    return <h1>Home</h1>;
+  if (!userPage?.params?.user || userPage?.pathnameBase === "/error") {
+    return null;
   }
 
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to={`/${pathname.split("/")[1]}`}>Acceuil</NavLink>
+          <NavLink to={`/${userPage.params.user}`}>Acceuil</NavLink>
         </li>
         <li>
-          <NavLink to={`/${pathname.split("/")[1]}/profile`}>Profil</NavLink>
+          <NavLink to={`/${userPage.params.user}/profile`}>Profil</NavLink>
         </li>
         <li>
-          <NavLink to={`/${pathname.split("/")[1]}/settings`}>Réglages</NavLink>
+          <NavLink to={`/${userPage.params.user}/settings`}>Réglages</NavLink>
         </li>
         <li>
-          <NavLink to={`/${pathname.split("/")[1]}/community`}>
+          <NavLink to={`/${userPage.params.user}/community`}>
             Communauté
           </NavLink>
         </li>
