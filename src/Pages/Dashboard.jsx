73757094567f4d344d3f2546a1cb230b01a5f437
Peally <pieclandre@gmail.com>
2023-06-18 21:18:45 +0200

    Feat : Dashboard catch data from hooks from store

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,8 +1,35 @@
+import { useContext } from "react";
 import AsideNav from "../Components/Navigation/AsideNav";
+import { DataContext } from "../utils/context/ContextProvider";
+import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader/Loader";
+
 const Dashboard = () => {
+  const Store = useContext(DataContext);
+  const { user } = useParams("/user");
+
+  const [userData, userDataLoading] = Store.data.useUserData(user);
+  const [userActivity, userActivityLoading] = Store.data.useUserActivity(user);
+  const [userPerformance, userPerformanceLoading] =
+    Store.data.useUserPerformance(user);
+  const [userAverageSessions, userAverageSessionsLoading] =
+    Store.data.useUserAverageSessions(user);
+
   return (
-    <main>
+    <main className="dashboard">
       <AsideNav />
+      <div className="container">
+        {userDataLoading ? (
+          <Loader />
+        ) : (
+          <h1>
+            <span>
+              Bonjour <strong>{userData.data.userInfos.firstName}</strong>
+            </span>
+            Félicitation ! Vous avez explosé vos objectifs hier 👏
+          </h1>
+        )}
+      </div>
     </main>
   );
 };
