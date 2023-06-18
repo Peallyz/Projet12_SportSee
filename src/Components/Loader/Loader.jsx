cfda2cca1764b6c4a526b7c28371ea0b60f5e2fe
Peally <pieclandre@gmail.com>
2023-06-18 21:16:00 +0200

    feat : loader

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__section"></div>
+      <div className="loader__section"></div>
+      <div className="loader__section"></div>
+      <div className="loader__section"></div>
+    </div>
+  );
+};
+
+export default Loader;
