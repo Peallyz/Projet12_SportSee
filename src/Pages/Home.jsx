6b67007d16e4333a1350b009abfb14294dbf45a2
Peally <pieclandre@gmail.com>
2023-06-18 21:19:20 +0200

    Refacto : no need to pass props

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,15 @@
-import { useUserData } from "../utils/hooks/fetchDataAPI";
 import UserSelect from "../Components/UserSelect/UserSelect";
-import PropTypes from "prop-types";
 
-const Home = ({ setPath }) => {
-  const [firstUser, firstLoading] = useUserData(12, true);
-  const [secondUser, secondLoading] = useUserData(18, true);
-
+const Home = () => {
   return (
-    <main>
+    <main className="home">
+      <h1>Veuillez selectionner un utilisteur</h1>
       <nav className="selector">
-        {!firstLoading ? (
-          <UserSelect data={firstUser} setPath={setPath} />
-        ) : (
-          "nope"
-        )}
-        {!secondLoading ? (
-          <UserSelect data={secondUser} setPath={setPath} />
-        ) : (
-          "nope"
-        )}
+        <UserSelect user={12} />
+        <UserSelect user={18} />
       </nav>
     </main>
   );
-};
-
-Home.propTypes = {
-  setPath: PropTypes.func,
 };
 
 export default Home;
