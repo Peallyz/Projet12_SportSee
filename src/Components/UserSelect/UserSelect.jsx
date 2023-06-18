f6c1eeac056da8fca0c227b144b0019a26b9dc7b
Peally <pieclandre@gmail.com>
2023-06-18 21:18:22 +0200

    Reafacto : UserSelect now use Store instead of props to escape props drilling

--- src/Components/UserSelect/UserSelect.jsx
+++ src/Components/UserSelect/UserSelect.jsx
@@ -1,19 +1,27 @@
 import PropTypes from "prop-types";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { DataContext } from "../../utils/context/ContextProvider";
+import Loader from "../Loader/Loader";
 
-const UserSelect = ({ data, setPath }) => {
-  return (
-    <NavLink to={`/${data.data.id}`} onClick={() => setPath(data.data.id)}>
+const UserSelect = ({ user }) => {
+  const Store = useContext(DataContext);
+
+  const [data, loading] = Store.data.useUserData(user);
+
+  return !loading ? (
+    <NavLink to={`/${data.data.id}`}>
       <span>{data.data.userInfos.firstName}</span>
       <span className="lastname">{data.data.userInfos.lastName}</span>
       <span>Utilisateur : {data.data.id}</span>
     </NavLink>
+  ) : (
+    <Loader />
   );
 };
 
 UserSelect.propTypes = {
-  data: PropTypes.object,
-  setPath: PropTypes.func,
+  user: PropTypes.number.isRequired,
 };
 
 export default UserSelect;
