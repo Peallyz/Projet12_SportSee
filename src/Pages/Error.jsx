9cb5f15417bb835421b4c3219dc05196e6e2c55e
Peally <pieclandre@gmail.com>
2023-06-18 21:18:57 +0200

    Feat : content on error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,15 @@
+import { NavLink } from "react-router-dom";
+import AsideNav from "../Components/Navigation/AsideNav";
 const Error = () => {
-  return <div></div>;
+  return (
+    <main>
+      <AsideNav />
+      <div className="errorPage">
+        <h1>Désolé, cette page n&apos;existe pas.</h1>
+        <NavLink to={"/"}>Revenir au choix des utilisateurs</NavLink>
+      </div>
+    </main>
+  );
 };
 
 export default Error;
