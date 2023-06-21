f6aaa6160d8d23068580a59ab295a40ae80254cd
Peally <pieclandre@gmail.com>
2023-06-21 20:44:12 +0200

    fix : first redirection on error page if no user select, now redirect to User Select

--- src/Components/Navigation/MainNav.jsx
+++ src/Components/Navigation/MainNav.jsx
@@ -2,14 +2,19 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMatch } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-// import { useParams } from "react-router-dom";
 
 const MainNav = () => {
-  // const { user } = useParams();
   const userPage = useMatch("/:user/*");
   const navigate = useNavigate();
   useEffect(() => {
-    if (userPage?.params?.user != "12" && userPage?.params?.user != "18") {
+    if (!userPage?.params?.user) {
+      navigate("/");
+    }
+    if (
+      userPage &&
+      userPage?.params?.user != "12" &&
+      userPage?.params?.user != "18"
+    ) {
       navigate("/error");
     }
   }, []);
