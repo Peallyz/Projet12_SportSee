77694d4aa74490e6755a3dcf7f92b9e249a6844f
Peally <pieclandre@gmail.com>
2023-06-23 10:23:23 +0200

    removing header and contexte form Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,24 +3,19 @@ import Home from "./Pages/Home";
 import Dashboard from "./Pages/Dashboard";
 import WorkInProgress from "./Pages/WorkInProgress";
 import Error from "./Pages/Error";
-import Header from "./Components/Header";
-import ContextProvider from "./utils/context/ContextProvider";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <ContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:user" element={<Dashboard />} />
-          <Route path="/:user/profile" element={<WorkInProgress />} />
-          <Route path="/:user/settings" element={<WorkInProgress />} />
-          <Route path="/:user/community" element={<WorkInProgress />} />
-          <Route path="/error" element={<Error />} />
-          <Route path="*" element={<Navigate to="/error" />} />
-        </Routes>
-      </ContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:user" element={<Dashboard />} />
+        <Route path="/:user/profile" element={<WorkInProgress />} />
+        <Route path="/:user/settings" element={<WorkInProgress />} />
+        <Route path="/:user/community" element={<WorkInProgress />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Navigate to="/error" />} />
+      </Routes>
     </BrowserRouter>
   );
 };
